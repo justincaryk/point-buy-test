e2bb0f4853ad63901bf986bf156774bc27ef0276
Justin Caryk <cary9983@gmail.com>
2020-01-08 23:15:02 -0800

    pretty sure this fool works...

--- src/Components/AsiGenerator/index.js
+++ src/Components/AsiGenerator/index.js
@@ -16,7 +16,6 @@ class AsiGenerator extends React.Component {
             },
             pointsUsed: 0,
             pointsRemaining: 27,
-            totalPointsAvail: 27,
             standardPointPool: 27,
             standardPointCost: {
                 // 18: 19,
@@ -36,112 +35,195 @@ class AsiGenerator extends React.Component {
                 // 4: -6,
                 // 3: -9,
             }
-        }
+        };
 
         this.attributes = [
             {
                 id: 1,
                 name: 'Strength',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
             {
                 id: 2,
                 name: 'Dexterity',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
             {
                 id: 3,
                 name: 'Constitution',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
             {
                 id: 4,
                 name: 'Intelligence',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
             {
                 id: 5,
                 name: 'Wisdom',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
             {
                 id: 6,
                 name: 'Charisma',
+                currentAssignedScore: 8,
+                previousAssignedScore: 8,
             },
-        ]
+        ];
+
+        this.handleChanges = (event) => {
+            //value format is [ASI_SCORE-ATTR_ID]
+            //after splitting this var will be ['ASI_SCORE', 'ATTRIBUTE_ID']
+            const eventValArr = event.target.value.split('-');
+
+            const attributeId = parseInt(eventValArr[1]);
+
+            for (let attr of this.attributes) {
+                if (attributeId == attr.id) {
+                    const newAttributeScore = parseInt(eventValArr[0]);
+                    const prevAttributeScore = parseInt(attr.previousAssignedScore);
+
+                    // if the previous score was 8, then it can be treated as a brand new selection
+                    const userMadeBrandNewSelection = prevAttributeScore == 8 ? true : false;
+                    // therefore, we assign the new choice as the previousScore for next time 
+                    if (userMadeBrandNewSelection) {
+                        // and simply decrease the remaining points
+                        this.setState({
+                            pointsRemaining: this.state.pointsRemaining - this.state.standardPointCost[newAttributeScore],
+                        })
+
+                        //update previous with current
+                        attr.previousAssignedScore = newAttributeScore;
+
+                        return;
+                    }
+
+                    // if there was a previous score, then we need to find out whether they increased or decreased
+                    // and then add or subtract the difference 
+                    const userIncreasedScore = Math.sign(newAttributeScore - prevAttributeScore) == 1 ? true : false;
+
+                    const pointCost = this.state.standardPointCost
+
+                    if (userIncreasedScore) {
+                        // new higher price - the previous lower price = real cost increase
+                        const debit = pointCost[newAttributeScore] - pointCost[prevAttributeScore];
+                        // subtract the additional cost from the pointsRemaining
+                        this.setState({
+                            pointsRemaining: this.state.pointsRemaining - debit,
+
+                        })
+                        //update previous with current
+                        attr.previousAssignedScore = newAttributeScore;
+                        return;
+                    }
+
+                    // previous higher price - new lower price = real cost decrease
+                    const credit = pointCost[prevAttributeScore] - pointCost[newAttributeScore];
+                    // add the credit back to pointsRemaining
+                    this.setState({
+                        pointsRemaining: this.state.pointsRemaining + credit,
+                    })
+                    //update previous with current
+                    attr.previousAssignedScore = newAttributeScore;
+
+                    return;
+                }
+            }
+
+
+        }
+
     }
 
+
+
     render() {
-
-
 
 
         return (
             <div className="content-wrap">
                 <div>
                     <div className="asi-heading">Points Remaining</div>
-                    <div className="asi-points-val-total">{this.state.pointsRemaining} / {this.state.totalPointsAvail}</div>
+                    <div className="asi-points-val-total">{this.state.pointsRemaining} / {this.state.standardPointPool}</div>
                 </div>
                 <div className="mt-20">
-                    <div className="big-ole-table-outer">
-                        <div className="tbl-row">
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Strength</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[0].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                    <form onChange={this.handleChanges}>
+                        <div className="big-ole-table-outer">
+                            <div className="tbl-row">
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Strength</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[0]}
+                                            key={this.attributes[0].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Dexterity</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[1].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Dexterity</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[1]}
+                                            key={this.attributes[1].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Constitution</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[2].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Constitution</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[2]}
+                                            key={this.attributes[2].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Intelligence</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[3].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Intelligence</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[3]}
+                                            key={this.attributes[3].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Wisdom</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[4].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Wisdom</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[4]}
+                                            key={this.attributes[4].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tbl-cell">
-                                <div className="asi-heading">Charisma</div>
-                                <div className="asi-val-select-outer">
-                                    <AttributeInputBlock
-                                        key={this.attributes[5].id}
-                                        availablePoints={this.state.pointsRemaining}
-                                        pointCosts={this.state.standardPointCost} >
-                                    </AttributeInputBlock>
+                                <div className="tbl-cell">
+                                    <div className="asi-heading">Charisma</div>
+                                    <div className="asi-val-select-outer">
+                                        <AttributeInputBlock
+                                            attribute={this.attributes[5]}
+                                            key={this.attributes[5].id}
+                                            availablePoints={this.state.pointsRemaining}
+                                            pointCosts={this.state.standardPointCost} >
+                                        </AttributeInputBlock>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         )
--- src/Components/AttributeInput/index.js
+++ src/Components/AttributeInput/index.js
@@ -3,28 +3,51 @@ import './AttributeInput.css';
 
 
 class AttributeInputBlock extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            availablePoints: this.props.availablePoints,
-            pointCosts: this.props.pointCosts,
+            attributeName: this.props.attributeName,
+            hasValueSelected: false,
+            indexOfOptionSelected: 0,
+            options: buildAvailableOptions(this)
         }
-        
+
+        this.handleChange = event => {
+            // const name = this.state.attributeName;
+            const newAssignedScore = event.target.value.split('-')[0];
+            // we'll need to set this so we can strip more expensive options if user runs out of points elsewhere
+            this.state.indexOfOptionSelected = _getIndexOfOptions(event.target.value, this.state.options);
+            //using this as a callback of sorts.. not sure whether this is reliable...
+            this.props.attribute.currentAssignedScore = parseInt(newAssignedScore);
+            //used as a flag to ensure options aren't reset when a user selected a value
+            this.state.hasValueSelected = newAssignedScore != 8 ? true : false;
+        }
     }
 
-    
-    
-
     render() {
-        
-        const options = buildAvailableOptions(this);
-        
-        options.join('');
+
+        let currentOptions;
+
+        // nothing is selected here, but other attributes have changed
+        if (_fullRebuildIsNeeded(this)) {
+            currentOptions = _rebuildAvailableOptions(this);
+        }
+        // something is selected here, but it isn't the highest possible 
+        // and splicing more expensive options may be required
+        else if (_splicingMoreExpensiveOptionsMayBeRequired(this)) {
+            const name = this.props.attribute.name;
+            currentOptions = _spliceAvailableOptions(this);
+        }
+        // else keep as is.
+        else {
+            currentOptions = this.state.options;
+        }
 
         return (
             <div>
-                <select className="select-css">{options}</select>
+                <select className="select-css"
+                    onChange={this.handleChange}>{currentOptions}</select>
             </div>
         )
 
@@ -32,10 +55,13 @@ class AttributeInputBlock extends React.Component {
 
 }
 
+export default AttributeInputBlock;
+
+
+// creating and modifying the options array
 function buildAvailableOptions(that) {
-    
-    let availablePoints = that.state.availablePoints;
-    const pointCosts = that.state.pointCosts;
+    let availablePoints = that.props.availablePoints;
+    const pointCosts = that.props.pointCosts;
     const pointDisplayVals = Object.keys(pointCosts);
 
     let lastPointKey = 8;
@@ -45,21 +71,109 @@ function buildAvailableOptions(that) {
     for (const opt of pointDisplayVals) {
         let optionHmtl;
 
-        if (availablePoints - pointCosts[lastPointKey]) {
+        if (Math.sign(availablePoints - pointCosts[lastPointKey]) != -1) {
             optionHmtl = (
-                <option value={pointCosts[lastPointKey]} key={pointCosts[lastPointKey]*lastPointKey}>{lastPointKey}</option>
+                <option
+                    value={lastPointKey + '-' + that.props.attribute.id}
+                    key={lastPointKey}>{lastPointKey}</option>
             );
             optionsArr.push(optionHmtl);
         }
-        
-        availablePoints = availablePoints - pointCosts[lastPointKey];
 
         lastPointKey++;
     }
-    
+
     return optionsArr;
 }
 
+function _rebuildAvailableOptions(that) {
+
+    if (!that.state && that.state.hasValueSelected == false) {
+        return;
+    }
+
+    let availablePoints = that.props.availablePoints;
+    const pointCosts = that.props.pointCosts;
+    const pointDisplayVals = Object.keys(pointCosts);
+
+    let lastPointKey = 8;
+
+    const optionsArr = [];
+
+    for (const opt of pointDisplayVals) {
+        let optionHmtl;
+
+        if (Math.sign(availablePoints - pointCosts[lastPointKey]) != -1) {
+            optionHmtl = (
+                <option
+                    value={lastPointKey + '-' + that.props.attribute.id}
+                    key={lastPointKey}>{lastPointKey}</option>
+            );
+            optionsArr.push(optionHmtl);
+        }
+
+        lastPointKey++;
+    }
+
+    return optionsArr;
+}
+
+function _spliceAvailableOptions(that) {
+
+    // current asi value
+    const currentAssignedScore = that.props.attribute.currentAssignedScore
+    const options = that.state.options;
+    //starting index
+    const startingIndex = that.state.indexOfOptionSelected;
+    //avaiable points
+    const availablePoints = that.props.availablePoints;
+    //cost of scores
+    const pointCosts = that.props.pointCosts;
+    
+    for (let i = startingIndex; i < options.length; i++) {
+        let attributeScoreOfCurrentOption = options[i].key;
+
+        if (availablePoints < pointCosts[attributeScoreOfCurrentOption]) {
+            options.length = i + 1;
+            break;
+        }
+    }
+    
+    return options;
+}
+
+// getting the index of the currently selected option
+function _getIndexOfOptions(eventTargetVal, optionsInState) {
+
+    for (let i = 0; i < optionsInState.length; i++) {
+
+        const opt = optionsInState[i];
+
+        if (eventTargetVal === opt.props.value) {
+            return i;
+        }
+
+    }
+}
 
 
-export default AttributeInputBlock;
+// basic validation funnels
+function _fullRebuildIsNeeded(that) {
+    if (that.props.availablePoints < 27 && that.state.hasValueSelected == false) {
+        return true;
+    }
+    return false;
+}
+
+function _splicingMoreExpensiveOptionsMayBeRequired(that) {
+
+    if (
+        that.state.hasValueSelected &&
+        that.state.indexOfOptionSelected < 7 &&
+        that.props.availablePoints < 27
+    ) {
+        return true;
+    }
+
+    return false;
+}
